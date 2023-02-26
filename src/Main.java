db7c0df67b21d77e3b649d438f55ce45d3e058a4
Gongada Mukesh <73887170+MukeshGongada22@users.noreply.github.com>
2023-02-26 18:26:11 +0530

    Java Code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=s.nextInt();
+        int j;
+        for(int i=1;i<=n;i++){
+            for(j=1;j<=n;j++) {
+                if (i == j) {
+                    System.out.print("*");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            j--;
+                System.out.print("*");
+
+                while(j>=1){
+                    if(i==j) {
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print("0");
+                    }
+                    j--;
+                }
+                System.out.println(" ");
+            }
+        }
     }
-}
